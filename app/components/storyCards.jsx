b95da0f6a8f72738c0a7f5b3fa5542684090297a
Huv4 <f.konto@posteo.de>
2024-04-27 07:26:08 +0200

    working date format

--- app/components/storyCards.jsx
+++ app/components/storyCards.jsx
@@ -3,11 +3,16 @@ import { getAllStories } from "@/lib/api";
 import Image from "next/image";
 import Link from "next/link";
 import { draftMode } from "next/headers";
-import { format, parse } from "date-fns";
 
 export default async function Home() {
   const { isEnabled } = draftMode();
   const stories = await getAllStories();
+  // Function to parse and format ISO8601 date
+  const formatDate = (isoDate) => {
+    const parsedDate = new Date(isoDate);
+    const options = { month: "long", day: "numeric", year: "numeric" };
+    return parsedDate.toLocaleDateString("de", options);
+  };
 
   return (
     <div className="grid grid-cols-1 gap-2 center-items justify-items-center md:grid-cols-2 xl:grid-cols-3">
@@ -17,7 +22,7 @@ export default async function Home() {
             <div className="relative opacity-100">
               <h1 className="z-10">{story.title}</h1>
               <h2>{story.photographer.name}</h2>
-              <h3>format({story.dateOfShot},"MM/dd/yyyy")</h3>
+              <h3>{formatDate(story.dateOfShot)}</h3>
               <p> </p>
               <div className="p-14">
                 <div className="bg-white p-2 shadow-md dark:bg-graylight dark:shadow-graylight">
